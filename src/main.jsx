76ef832a366de0ac48164eec842209c9ebb4bc9d
Safvan P <pcsajitha87@gmail.com>
2026-02-12 16:52:47 +0530

    rebuild with env

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { BrowserRouter } from "react-router-dom"; 
-import "./index.css"; 
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>  
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
