2d14983efea5af02ece36ee7383eeeab8271cb19
Safvan P <pcsajitha87@gmail.com>
2026-02-12 16:41:52 +0530

    added base path

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { HashRouter } from "react-router-dom";
-import "./index.css";
-
+import { BrowserRouter } from "react-router-dom"; 
+import "./index.css"; 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>  
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
